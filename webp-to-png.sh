dd8443d9b553c546635d5bf2e0037ca1fe9d5605
W M Hill <123903924+w-m-hill@users.noreply.github.com>
2023-03-04 13:38:05 -0800

    Add files via upload

---
+++ webp-to-png.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Load configuration from file
+source ./config.cfg
+
+# Create output directory if it does not exist
+mkdir -p "$OUTPUT_DIR"
+
+# Monitor input directory for new .webp files
+inotifywait -m -r -e create "$INPUT_DIR" --format '%w%f' |
+while read file; do
+    # Check if file is a .webp image
+    if [[ "$file" == *.webp ]]; then
+        # Convert file to .png
+        filename=$(basename "$file")
+        pngfile="${filename%.*}.png"
+        dwebp "$file" -o "$OUTPUT_DIR/$pngfile"
+        
+        # Delete original .webp file if option is set
+        if [[ "$DELETE_WEBP" == "true" ]]; then
+            rm "$file"
+        fi
+    fi
+done
+
